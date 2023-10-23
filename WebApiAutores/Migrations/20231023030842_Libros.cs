4830d8e2019b7846cbdaf02e677912f726fc76aa
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2023-10-22 22:26:51 -0500

    Add project files.

---
+++ WebApiAutores/Migrations/20231023030842_Libros.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiAutores.Migrations
+{
+    /// <inheritdoc />
+    public partial class Libros : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Libro_Autor_AutorId",
+                table: "Libro");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Libro",
+                table: "Libro");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Autor",
+                table: "Autor");
+
+            migrationBuilder.RenameTable(
+                name: "Libro",
+                newName: "Libros");
+
+            migrationBuilder.RenameTable(
+                name: "Autor",
+                newName: "Autores");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Libro_AutorId",
+                table: "Libros",
+                newName: "IX_Libros_AutorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Libros",
+                table: "Libros",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Autores",
+                table: "Autores",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Libros_Autores_AutorId",
+                table: "Libros",
+                column: "AutorId",
+                principalTable: "Autores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Libros_Autores_AutorId",
+                table: "Libros");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Libros",
+                table: "Libros");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Autores",
+                table: "Autores");
+
+            migrationBuilder.RenameTable(
+                name: "Libros",
+                newName: "Libro");
+
+            migrationBuilder.RenameTable(
+                name: "Autores",
+                newName: "Autor");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Libros_AutorId",
+                table: "Libro",
+                newName: "IX_Libro_AutorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Libro",
+                table: "Libro",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Autor",
+                table: "Autor",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Libro_Autor_AutorId",
+                table: "Libro",
+                column: "AutorId",
+                principalTable: "Autor",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
